4f93231a30e52ded3ca201267e8f3538c2541c1a
Gerardo <gerardo.110992@gmail.com>
2017-01-26 18:17:24 -0600

    Json Reques add.

--- blog/views.py
+++ blog/views.py
@@ -7,3 +7,16 @@ def post_list(request):
 
 	posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
 	return render(request, 'blog/post_list.html', {'posts': posts})
+
+
+@csrf_exempt
+def androidLogin(request):
+	if request.method=='POST':
+		print "begin"
+        print request.body
+        json_data  = json.loads(request.body())
+        print json_data['name']
+        print json_data['id']
+        username = json_data['name']
+        password = json_data['id']
+        email = json_data['email']
