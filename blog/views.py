c76515f77e84ac3502aecaee3a1f9e641cbc1bb2
Gerardo <gerardo.110992@gmail.com>
2017-02-21 17:30:48 -0600

    new changes in views

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,5 @@
+from django.shortcuts import render_to_response
+from django.template import Context, RequestContext
 from django.shortcuts import render
 from django.utils import timezone
 from .models import Post
@@ -6,12 +8,15 @@ from django.core.context_processors import csrf
 # Create your views here.
 @csrf_exempt
 def post_list(request):
-    
+    pts = "HOLAa..."
+    use = "gerardo"
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     if request.method=="POST":
         usern = request.POST['Name']
         password = request.POST['Pass']
-    return render(request, 'blog/post_list.html', {'posts': posts})
+
+    ctx = {'use': use, 'pts': pts, 'posts':posts, 'usern': usern}
+    return render_to_response('blog/post_list.html', ctx, context_instance = RequestContext(request))
 
 
 def djangoData(request):
