ffc7582ecd69f175f0b909b23be2c404f0eecce2
Gerardo <gerardo.110992@gmail.com>
2017-02-21 15:27:26 -0600

    add get request print

--- blog/views.py
+++ blog/views.py
@@ -6,16 +6,12 @@ from django.core.context_processors import csrf
 # Create your views here.
 @csrf_exempt
 def post_list(request):
-
-
+    
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
-
     if request.method=="POST":
-
         usern = request.POST['Name']
         password = request.POST['Pass']
-
-	return render(request, 'blog/post_list.html', {'posts': posts})
+    return render(request, 'blog/post_list.html', {'posts': posts})
 
 
 def djangoData(request):
